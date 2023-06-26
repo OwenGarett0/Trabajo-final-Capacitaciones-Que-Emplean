77bab8ad4c71483248a799f2c5d5552e950d13b6
FNCasd <fnardellicastroviejo@escuelasproa.edu.ar>
2023-06-26 19:50:37 -0300

    Implementación de documentos html

--- Frontend/src/app/modulo-inicio/modulo-inicio.component.ts
+++ Frontend/src/app/modulo-inicio/modulo-inicio.component.ts
@@ -1,27 +1,10 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-modulo-inicio',
   templateUrl: './modulo-inicio.component.html',
   styleUrls: ['./modulo-inicio.component.css']
 })
 export class ModuloInicio {
-  constructor(private router: Router) { }
-
-  goToLogin() {
-    this.router.navigate(['/login']);
-  }
-  goToRegister() {
-    this.router.navigate(['/register']);
-  }
-  goToInfo() {
-    this.router.navigate(['/info'])
-  }
-  goToForum() {
-    this.router.navigate(['/forum'])
-  }
-  goToStats() {
-    this.router.navigate(['/stats'])
-  }
   
 }
